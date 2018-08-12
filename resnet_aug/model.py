eab7d2b52f7356e6aeecf471b25d8d5925fbd791
X-Lai <laixin112233@qq.com>
2018-08-12 15:32:26 +0800

    add resnet_aug

---
+++ resnet_aug/model.py
@@ -0,0 +1,130 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+class ResNet(nn.Module):
+    def __init__(self, channels, layers, same_shape=True):
+        super(ResNet, self).__init__()
+        self.resnet = nn.ModuleList([])
+        self.resnet.append(Residual(channels, same_shape=same_shape))
+        for it in range(layers-1):
+            self.resnet.append(Residual(channels, same_shape=True))
+    def forward(self, x):
+        out = x
+        for resnet in self.resnet:
+            out = resnet(out)
+        return out
+
+class Residual(nn.Module):
+    def __init__(self, channels, same_shape=True, weight_init=True):
+        super(Residual, self).__init__()
+        stride = 1
+        in_channels = channels
+        self.same_shape = same_shape
+        if not same_shape:
+            stride = 2
+            in_channels = channels // 2
+            self.conv3 = nn.Conv2d(in_channels=in_channels, out_channels=channels,
+                                   kernel_size=1, stride=stride)
+        self.bn1 = nn.BatchNorm2d(in_channels)
+        self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=channels,
+                               kernel_size=3, stride=stride, padding=1)
+        self.bn2 = nn.BatchNorm2d(channels)
+        self.conv2 = nn.Conv2d(in_channels=channels, out_channels=channels,
+                               kernel_size=3, stride=1, padding=1)
+        if weight_init:
+            nn.init.kaiming_normal_(self.conv1.weight, nonlinearity='relu')
+            nn.init.kaiming_normal_(self.conv2.weight, nonlinearity='relu')
+            if not same_shape:
+                nn.init.kaiming_normal_(self.conv3.weight, nonlinearity='relu')
+    def forward(self, x):
+        out = self.conv1(F.relu(self.bn1(x)))
+        out = self.conv2(F.relu(self.bn2(out)))
+        if self.same_shape:
+            return out + x
+        return out + self.conv3(x)
+
+class conv_relu_pool(nn.Module):
+    def __init__(self, in_channel, out_channel, conv_kernel_size, conv_stride, conv_padding,
+                 pool_kernel_size, pool_stride, pool_padding, bn=True, dropout=0):
+        super(conv_relu_pool, self).__init__()
+        self.conv = nn.Conv2d(in_channels=in_channel, out_channels=out_channel,
+                              kernel_size=conv_kernel_size, stride=conv_stride,
+                              padding=conv_padding)
+        self.BN = bn
+        if bn:
+            self.bn = nn.BatchNorm2d(out_channel)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout2d(dropout)
+        self.pool = nn.MaxPool2d(kernel_size=pool_kernel_size, stride=pool_stride,
+                                 padding=pool_padding)
+
+    def forward(self, x):
+        conv = self.conv(x)
+        if self.BN:
+            conv = self.bn(conv)
+        relu = self.relu(conv)
+        relu = self.dropout(relu)
+        pool = self.pool(relu)
+        return pool
+
+class conv_relu_conv_relu_pool(nn.Module):
+    def __init__(self, in_channel, mid_channel, kernel_size1, stride1, padding1,
+                 out_channel, kernel_size2, stride2, padding2,
+                 pool_kernel_size, pool_stride, pool_padding, bn=True, dropout=0):
+        super(conv_relu_conv_relu_pool, self).__init__()
+        self.conv = nn.Conv2d(in_channels=in_channel, out_channels=mid_channel,
+                              kernel_size=kernel_size1, stride=stride1, padding=padding1)
+        self.BN = bn
+        if bn:
+            self.bn = nn.BatchNorm2d(out_channel)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout2d(dropout)
+        self.conv_relu_pool = conv_relu_pool(in_channel=mid_channel, out_channel=out_channel,
+                                             conv_kernel_size=kernel_size2, conv_stride=stride2,
+                                             conv_padding=padding2, pool_kernel_size=pool_kernel_size,
+                                             pool_stride=pool_stride, pool_padding=pool_padding,
+                                             bn=bn, dropout=dropout)
+
+    def forward(self, x):
+        conv = self.conv(x)
+        if self.BN:
+            conv = self.bn(conv)
+        relu = self.relu(conv)
+        relu = self.dropout(relu)
+        result = self.conv_relu_pool(relu)
+        return result
+
+class bn_relu_conv(nn.Module):
+    def __init__(self, in_channel, out_channel, kernel_size=3, stride=1, padding=1, dropout=0):
+        super(bn_relu_conv, self).__init__()
+        self.bn = nn.BatchNorm2d(in_channel)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(dropout)
+        self.conv = nn.Conv2d(in_channels=in_channel, out_channels=out_channel,
+                              kernel_size=kernel_size, stride=stride, padding= padding)
+
+    def forward(self, x):
+        bn = self.bn(x)
+        relu = self.relu(bn)
+        dropout = self.dropout(relu)
+        conv = self.conv(dropout)
+        return conv
+
+class affine_relu(nn.Module):
+    def __init__(self, in_dim, out_dim, bn=True, dropout=0):
+        super(affine_relu, self).__init__()
+        self.affine = nn.Linear(in_dim, out_dim)
+        self.BN = bn
+        if bn == True:
+            self.bn = nn.BatchNorm1d(out_dim)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(dropout)
+    def forward(self, x):
+        affine = self.affine(x)
+        if self.BN:
+            affine = self.bn(affine)
+        return self.dropout(self.relu(affine))
+
+class Flatten(nn.Module):
+    def forward(self, x):
+        return x.view(x.size(0), -1)
---
+++ resnet_aug/train.py
@@ -0,0 +1,108 @@
+from helper import _check_acc, Mode
+import random
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, sampler
+from dataset import cifar10
+import torchvision.transforms as T
+import model as model
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+from tensor_transforms import Pad, RandomFlip, RandomCrop
+import numpy as np
+import matplotlib.pyplot as plt
+plt.switch_backend('agg')
+
+def check_accuracy(model):
+    if model.training:
+        train_acc = _check_acc(model, loader_train_test, Mode.train, device=device, dtype=dtype)
+        val_acc = _check_acc(model, loader_val, Mode.val, device=device, dtype=dtype)
+        a.append(train_acc)
+        b.append(val_acc)
+        return val_acc
+    else:
+        _check_acc(model, loader_test, Mode.test, device=device, dtype=dtype)
+
+
+def train(model, optimizer, epochs, print_every=1000, plot_every=20):
+    scheduler = ReduceLROnPlateau(optimizer=optimizer, mode='max', factor=0.1, patience=10, verbose=True)
+    model = model.to(device=device)
+    for e in range(epochs):
+        for t, (x,y) in enumerate(loader_train):
+            model.train()
+            x = x.to(device=device, dtype=dtype)
+            y = y.to(device=device, dtype=torch.long)
+
+            scores = model(x)
+            _, preds = scores.max(1)
+            loss = F.cross_entropy(scores, y)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            if t % print_every == 0:
+                print('Epoch %d, Iteration %d, loss = %.4f' % (e, t, loss))
+                val_acc = check_accuracy(model)
+                scheduler.step(val_acc)
+                print()
+
+        if e % plot_every == 0 and e > 0:
+            plt.plot(np.arange(len(a)), a, 'b', np.arange(len(b)), b, 'y')
+            plt.ylim(0,1)
+            # plt.show()
+            plt.savefig("./logs/Epoch %d.png" % e)
+
+a = []
+b = []
+BATCH_SIZE = 128
+NUM_TRAIN = 49000
+
+USE_GPU = True
+if USE_GPU and torch.cuda.is_available():
+    device = torch.device('cuda')
+else:
+    device = torch.device('cpu')
+dtype = torch.float32
+
+transform = T.Compose([
+    T.ToTensor(),
+    T.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616)),
+    Pad(size=(3,36,36)),
+    RandomCrop(size=(32,32)),
+    RandomFlip(h=True),
+])
+
+dataset = cifar10('./cifar-10-batches-py', transform=transform)
+
+loader_train = DataLoader(dataset, batch_size=BATCH_SIZE,
+                          sampler=sampler.SubsetRandomSampler(range(NUM_TRAIN)))
+loader_train_test = DataLoader(dataset, batch_size=BATCH_SIZE,
+                               sampler=sampler.SubsetRandomSampler(random.sample(range(NUM_TRAIN), 1000)))
+loader_val = DataLoader(dataset, batch_size=BATCH_SIZE,
+                        sampler=sampler.SubsetRandomSampler(range(NUM_TRAIN, 50000)))
+loader_test = DataLoader(dataset, batch_size=BATCH_SIZE,
+                         sampler=sampler.SubsetRandomSampler(range(50000, 60000)))
+
+dropout = 0
+hidden1 = 128
+num_classes = 10
+
+model = nn.Sequential(
+    nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1),
+    model.ResNet(channels=16, layers=9, same_shape=True),
+    model.ResNet(channels=32, layers=9, same_shape=False),
+    model.ResNet(channels=64, layers=9, same_shape=False),
+    nn.BatchNorm2d(64),
+    nn.ReLU(),
+    nn.AvgPool2d(kernel_size=8),
+    model.Flatten(),
+    nn.Linear(64, 10)
+)
+
+nn.init.kaiming_normal_(model[0].weight, nonlinearity='relu')
+nn.init.kaiming_normal_(model[8].weight, nonlinearity='relu')
+
+optimizer = optim.SGD(model.parameters(), lr=0.1, momentum=0.9, weight_decay=1e-4, nesterov=True)
+
+train(model, optimizer, epochs=300)
